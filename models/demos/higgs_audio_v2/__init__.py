1c7f220bbe674320e3fdd9330d7d13bbd462cc2e
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-12 16:28:00 -0400

    fix: apply solution for issue #32068

--- models/demos/higgs_audio_v2/__init__.py
+++ models/demos/higgs_audio_v2/__init__.py
@@ -1,3 +1,1 @@
-from .higgs_audio_v2 import HiggsAudioV2, load_model, generate_audio
-from .tokenizer import tokenize_text, tokenize_audio
-from .audio_tokenizer import AudioTokenizer
+from .higgs_audio_v2 import HiggsAudioV2
