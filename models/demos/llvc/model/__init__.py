a5d67dd4f4c53b04000e225b45e2cebdaa248255
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-28 10:25:53 -0400

    fix: apply solution for issue #32187

---
+++ models/demos/llvc/model/__init__.py
@@ -0,0 +1,6 @@
+from .llvc_model import LLVCModel
+from .encoder import LightweightEncoder
+from .decoder import EfficientDecoder
+from .vocoder import Vocoder
+
+__all__ = ['LLVCModel', 'LightweightEncoder', 'EfficientDecoder', 'Vocoder']
