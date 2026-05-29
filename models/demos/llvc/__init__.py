56ee865d5f73086221c0f912e2a5cbb268caebb4
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-29 07:12:15 -0400

    fix: apply solution for issue #32187

---
+++ models/demos/llvc/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2024 Tenstorrent AI ULC
+
+from models.demos.llvc.tt_llvc import TtLLVC
+from models.demos.llvc.reference.llvc_pytorch import LlvcPytorch
