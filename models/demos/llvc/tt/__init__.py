63de15a755b774b5dfe2f5414791d3b6a3578a38
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-07-07 02:07:31 -0400

    fix: apply solution for issue #32187

--- models/demos/llvc/tt/__init__.py
+++ models/demos/llvc/tt/__init__.py
@@ -1,11 +1,15 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+
 # SPDX-License-Identifier: Apache-2.0
-#
-# Copyright (c) 2024 Tenstorrent AI ULC
 
-from .llvc_model import LLVCModel
-from .encoder import LLVCEncoder
-from .decoder import LLVCDecoder
-from .vocoder import LLVCVocoder
-from .config import LLVCConfig
+from models.demos.llvc.tt.llvc_model import TtLLVCModel
+from models.demos.llvc.tt.encoder import TtLLVCEncoder
+from models.demos.llvc.tt.decoder import TtLLVCDecoder
+from models.demos.llvc.tt.vocoder import TtLLVCVocoder
 
-__all__ = ["LLVCModel", "LLVCEncoder", "LLVCDecoder", "LLVCVocoder", "LLVCConfig"]
+__all__ = [
+    "TtLLVCModel",
+    "TtLLVCEncoder",
+    "TtLLVCDecoder",
+    "TtLLVCVocoder",
+]
